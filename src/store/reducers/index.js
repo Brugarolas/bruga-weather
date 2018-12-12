ddc0366d2b5a3aae8f5c407b457b2a52e1f67969
Andrés Brugarolas <brugarolas.92@gmail.com>
2018-12-12 12:42:36 +0100

    Remove duplicates
    Don't allow duplicates

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -5,9 +5,14 @@ const initialState = {
   locations: []
 };
 
+const idExists = (store, id) => {
+  return !!store.find(element => element.id === id);
+}
+
 const locationReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_LOCATION:
+      if (idExists(state.locations, action.payload.id)) return state;
       return { ...state, locations: [...state.locations, action.payload] };
     default:
       return state;
