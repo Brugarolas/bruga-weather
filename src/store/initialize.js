b221478f100e5e7018b0e15253f4c0f735fa398c
Andrés Brugarolas <brugarolas.92@gmail.com>
2019-01-16 16:07:28 +0100

    Auto update bugfix

--- src/store/initialize.js
+++ src/store/initialize.js
@@ -7,7 +7,11 @@ export default (store, locationStorage) => {
   }
 
   const update = () => {
-    store.dispatch(Actions.replaceLocations(locationStorage.getLocations()));
+    const locations = locationStorage.getLocations();
+    if (locations.length > 0) {
+      store.dispatch(Actions.replaceLocations(locations));
+    }
+    return locations.length;
   }
 
   setInterval(update, 300000) // 5 * 60 * 1000 ms = 5 minutes
